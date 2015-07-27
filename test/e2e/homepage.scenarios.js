81e6844842bb99a3684ca4b36fc4f0ce02c6a166
rodrigopavezi <rodrigopavezi@gmail.com>
2015-07-27 17:24:51 -0300

    Added landing page

--- test/e2e/homepage.scenarios.js
+++ test/e2e/homepage.scenarios.js
@@ -3,6 +3,8 @@ var expect = require('rv-common-e2e').expect;
 var HomePage = require('./pages/homepage.js');
 var CommonHeaderPage = require('rv-common-e2e').commonHeaderPage;
 var helper = require('rv-common-e2e').helper;
+var GoogleAuthPage = require('rv-common-e2e').googleAuthPage;
+
 
 
 browser.driver.manage().window().setSize(1920, 1080);
@@ -12,31 +14,95 @@ describe("In order to manage schedules " +
   this.timeout(2000);// to allow for protactor to load the seperate page
   var homepage;
   var commonHeaderPage;
+  var googleAuthPage;
   before(function (){
     homepage = new HomePage();
     commonHeaderPage = new CommonHeaderPage();
-    homepage.get();
-    helper.waitDisappear(commonHeaderPage.getLoader(), 'CH spinner loader').then(function () {
-      commonHeaderPage.signin();
+    googleAuthPage = new GoogleAuthPage();
+  });
+
+  describe("Given a user who access the schedules app", function() {
+
+    before(function (){
+      homepage.get();
+      //wait for spinner to go away.
+      helper.waitDisappear(commonHeaderPage.getLoader(), 'CH spinner loader');
+    });
+
+    it('should load', function () {
+      expect(homepage.getSchedulesAppContainer().isPresent()).to.eventually.be.true;
+    });
+
+    it('should load common header', function () {
+      expect(commonHeaderPage.getCommonHeader().isPresent()).to.eventually.be.true;
+    });
+
+    it('should have a schedule menu item on the common header', function () {
+      expect(commonHeaderPage.getCommonHeaderMenuItems().get(0).isPresent()).to.eventually.be.true;
+      expect(commonHeaderPage.getCommonHeaderMenuItems().get(0).getText()).to.eventually.equal('Schedules');
+    });
+
+    it('should go to home when clicking on Schedules menu item', function () {
+      commonHeaderPage.getCommonHeaderMenuItems().get(0).click();
+      expect(browser.getCurrentUrl()).to.eventually.equal(homepage.getUrl());
+    });
+
+    it('should show the schedules image', function () {
+      expect(homepage.getSchedulesImage().isPresent()).to.eventually.be.true;
+    });
+
+    it('should show the manage schedules text', function () {
+      expect(homepage.getManageSchedulesText().getText()).to.eventually.equal('Schedule and deliver your content to an unlimited numbers of displays, anywhere, anytime.');
+    });
+
+    it('should show the schedules explanation text', function () {
+      expect(homepage.getSchedulesExplanationText().getText()).to.eventually.equal('Schedules is where you setup timelines and distributions lists for your playlists of Presentations and HTML pages for your Displays.');
+    });
+
+    it('should show the Sign Up link', function () {
+      expect(homepage.getSignUpLink().isPresent()).to.eventually.be.true;
+    });
+
+    it('should show the Sign In link', function () {
+      expect(homepage.getSignInLink().isPresent()).to.eventually.be.true;
+    });
+
+    it('should show the Sign Up text', function () {
+      expect(homepage.getSignUpText().getText()).to.eventually.equal('for free, no credit card required, or');
+    });
+
+    it('should show the Sign In text', function () {
+      expect(homepage.getSignInText().getText()).to.eventually.equal('if you already have an account.');
     });
   });
 
-  it('should load',function(){
-    expect(homepage.getSchedulesAppContainer().isPresent()).to.eventually.be.true;
+  describe("Given a user who wants to sign up", function() {
+
+    before(function (){
+      homepage.get();
+      //wait for spinner to go away.
+      helper.waitDisappear(commonHeaderPage.getLoader(), 'CH spinner loader');
+    });
+
+    it('should open sign up model when clicking on the sign up link',function(){
+      homepage.getSignUpLink().click();
+      expect(commonHeaderPage.getModalDialog().isPresent()).to.eventually.be.true;
+    });
   });
 
-  it('should load common header',function(){
-    expect(commonHeaderPage.getCommonHeader().isPresent()).to.eventually.be.true;
-  });
+  describe("Given a user who wants to sign in", function() {
 
-  it('should have a schedule menu item on the common header',function(){
-   expect(commonHeaderPage.getCommonHeaderMenuItems().get(0).isPresent()).to.eventually.be.true;
-   expect(commonHeaderPage.getCommonHeaderMenuItems().get(0).getText()).to.eventually.equal('Schedules');
-  });
+    before(function (){
+      homepage.get();
+      //wait for spinner to go away.
+      helper.waitDisappear(commonHeaderPage.getLoader(), 'CH spinner loader');
+    });
 
-  it('should go to home when clicking on Schedules menu item',function(){
-    commonHeaderPage.getCommonHeaderMenuItems().get(0).click();
-    expect(browser.getCurrentUrl()).to.eventually.equal(homepage.getUrl()+"schedule/list?cid=ac57def2-834e-4ecd-8b91-44ca14524fd0");
+    it('should go to google authentication when clicking on the sign in link',function(){
+      homepage.getSignInLink().click().then(function () {
+        googleAuthPage.signin();
+        expect(browser.getCurrentUrl()).to.eventually.contain(homepage.getUrl());
+      });
+    });
   });
-
 });
--- test/e2e/pages/homepage.js
+++ test/e2e/pages/homepage.js
@@ -5,6 +5,14 @@ var HomePage = function() {
   var url = config.rootUrl + '/#/';
   var schedulesAppContainer = element(by.css('.schedules-app'));
 
+  var schedulesImage = element(by.id('schedules-image'));
+  var manageSchedulesText = element(by.id('manage-schedules-text'));
+  var schedulesExplanationText = element(by.id('schedules-explanation-text'));
+  var signUpText = element(by.id('sign-up-text'));
+  var signInText = element(by.id('sign-in-text'));
+  var signUpLink = element(by.id('sign-up-link'));
+  var signInLink = element(by.id('sign-in-link'));
+
   this.get = function() {
     browser.get(url);
   };
@@ -15,6 +23,34 @@ var HomePage = function() {
 
   this.getSchedulesAppContainer = function() {
     return schedulesAppContainer;
+  };
+
+  this.getSchedulesImage = function() {
+    return schedulesImage;
+  };
+
+  this.getManageSchedulesText = function() {
+    return manageSchedulesText;
+  };
+
+  this.getSchedulesExplanationText = function() {
+    return schedulesExplanationText;
+  };
+
+  this.getSignUpText = function() {
+    return signUpText;
+  };
+
+  this.getSignInText = function() {
+    return signInText;
+  };
+
+  this.getSignUpLink = function() {
+    return signUpLink;
+  };
+
+  this.getSignInLink = function() {
+    return signInLink;
   };
 };
 
